6552102f5e00707fb45f66f18fa148d056c90bd7
StealthWoosh <daffaikuganteng11@gmail.com>
2025-07-28 21:40:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Untuk demo sederhana
+document.querySelector('.am-btn').addEventListener('click', function () {
+  alert('Kamu sudah berada di About Me page.');
+});
+
+document.querySelector('.pi-btn').addEventListener('click', function () {
+  alert('Untuk kontak pribadi, masih coming soon ya.');
+});
+
+
